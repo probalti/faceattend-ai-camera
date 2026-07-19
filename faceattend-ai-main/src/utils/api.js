24e96b92b23e8223f1c4e8e4b5efadef63a7691e
WASIYA-123 <wasiyaamir786@gmail.com>
2026-07-19 18:17:23 +0500

    Update api.js 34

--- faceattend-ai-main/src/utils/api.js
+++ faceattend-ai-main/src/utils/api.js
@@ -3,7 +3,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  // baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: "https://faceattend-ai-production.up.railway.app",
   timeout: 15000,
 });
 
